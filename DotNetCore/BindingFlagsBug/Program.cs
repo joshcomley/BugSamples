8a1b1dc0621971479417a78569dd3d9914cdb93a
Josh Comley <Josh Comley>
2016-03-05 18:19:00 +0100

    Narrowed down source of bug

--- DotNetCore/BindingFlagsBug/Program.cs
+++ DotNetCore/BindingFlagsBug/Program.cs
@@ -6,7 +6,6 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             var bf = System.Reflection.BindingFlags.CreateInstance;
         }
     }
